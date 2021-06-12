f63153ec0340b221977c2651846dc52b95c98af9
Luzyla <luzyla@gmail.com>
2021-06-12 05:25:51 -0300

    Add Netlify status in Readme

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,15 +5,16 @@ import { FiArrowRight } from "react-icons/fi";
 import { FiArrowRightCircle } from "react-icons/fi";
 
 const Pagination = () => {
-//   const [pagination, setPagination] = useState([])
-
-//   useEffect(() => {
-//     fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=986cb57c124ba1dd4bd40f4efe74ae25')
-//     .then(res => res.json())
-//     .then(response => { 
-//         setPagination(response.results)   
-//     })
-//   }, [])
+  const [pagination, setPagination] = useState([])
+  console.log(pagination)
+  
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=986cb57c124ba1dd4bd40f4efe74ae25')
+    .then(res => res.json())
+    .then(response => { 
+        setPagination(response.results)   
+    })
+  }, [])
 
   return (
     
